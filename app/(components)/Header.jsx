618fba85546ac748f47b95a896da809ff6a0a589
The-LAW <joshuagboyega23@gmail.com>
2026-06-25 20:36:29 +0100

    made changes

--- app/(components)/Header.jsx
+++ app/(components)/Header.jsx
@@ -1,6 +1,5 @@
-import Link from 'next/link'
-import React from 'react'
-import { Button } from "@/components/ui/button"
+import Link from 'next/link';
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,48 +8,44 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
 
-function Header() {
+export default function Header() {
   return (
-    <header>
-        <div>
-            <div>
-                {/* LOGO */}
-                <div>
-                    <Link href={'/'}>
-                    <span>
-                        Shoppz<span>o</span>
-                    </span>
-                    </Link>
-                </div>
-                {/* SEARCH */}
-                <div>
-                    <div>
-                        <input type="text" placeholder='Type here...' className='' />
-<DropdownMenu>
-  <DropdownMenuTrigger asChild>
-    <Button variant="outline">Open</Button>
-  </DropdownMenuTrigger>
-  <DropdownMenuContent>
-    <DropdownMenuGroup>
-      <DropdownMenuLabel>My Account</DropdownMenuLabel>
-      <DropdownMenuItem>Profile</DropdownMenuItem>
-      <DropdownMenuItem>Billing</DropdownMenuItem>
-    </DropdownMenuGroup>
-    <DropdownMenuSeparator />
-    <DropdownMenuGroup>
-      <DropdownMenuItem>Team</DropdownMenuItem>
-      <DropdownMenuItem>Subscription</DropdownMenuItem>
-    </DropdownMenuGroup>
-  </DropdownMenuContent>
-</DropdownMenu>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
-  )
-}
+    <header className="border-b bg-white sticky top-0 z-50">
+      <div className="max-padd-container py-4">
+        <div className="flex items-center justify-between">
+          
+          {/* Logo */}
+          <Link href="/" className="text-2xl font-bold">
+            Shoppz<span className="text-blue-600">o</span>
+          </Link>
 
-export default Header
+          {/* Search Input */}
+          {/* <div className="flex-1 max-w-xl mx-8">
+            <input
+              type="text"
+              placeholder="Search products..."
+              className="w-full px-5 py-3 border border-gray-300 rounded-full focus:outline-none focus:border-blue-500 text-sm"
+            />
+          </div> */}
+
+          {/* Account Dropdown */}
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline">Account</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+    </header>
+  );
+}
--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
       <body
       className={`${inter.variable} ${knewave.variable} antialiased`}
       >
-      <Header />
+      {/* <Header /> */}
       {children}
       </body>
     </html>
